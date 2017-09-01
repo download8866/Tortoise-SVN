f3b6f0121b12803709be73bc44bd890e846f5ec6
julianfoad1 <julianfoad1@154be670-7969-47af-9ab0-c6fb378d2295>
2017-09-01 10:34:28 +0000

    Create a branch for client-side shelving UI.

---
+++ branches/shelve/src/TortoiseMerge/SetColorPage.cpp
@@ -0,0 +1,267 @@
+// TortoiseMerge - a Diff/Patch program
+
+// Copyright (C) 2006-2008, 2012-2014 - TortoiseSVN
+// Copyright (C) 2016 - TortoiseGit
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "stdafx.h"
+#include "TortoiseMerge.h"
+#include "SetColorPage.h"
+#include "DiffColors.h"
+
+
+// CSetColorPage dialog
+
+IMPLEMENT_DYNAMIC(CSetColorPage, CPropertyPage)
+CSetColorPage::CSetColorPage()
+    : CPropertyPage(CSetColorPage::IDD)
+    , m_bReloadNeeded(FALSE)
+    , m_regInlineAdded(L"Software\\TortoiseMerge\\InlineAdded", INLINEADDED_COLOR)
+    , m_regInlineRemoved(L"Software\\TortoiseMerge\\InlineRemoved", INLINEREMOVED_COLOR)
+    , m_regModifiedBackground(L"Software\\TortoiseMerge\\Colors\\ColorModifiedB", MODIFIED_COLOR)
+    , m_bInit(false)
+{
+}
+
+CSetColorPage::~CSetColorPage()
+{
+    m_bInit = FALSE;
+}
+
+static COLORREF GetColorFromButton(const CMFCColorButton& button)
+{
+    COLORREF col = button.GetColor();
+    if (col == -1)
+        return button.GetAutomaticColor();
+    return col;
+}
+
+void CSetColorPage::SaveData()
+{
+    if (!m_bInit)
+        return;
+
+    COLORREF cFg = GetColorFromButton(m_cFgNormal);
+    COLORREF cBk = GetColorFromButton(m_cBkNormal);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_NORMAL, cBk, cFg);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_UNKNOWN, cBk, cFg);
+
+    cFg = GetColorFromButton(m_cFgRemoved);
+    cBk = GetColorFromButton(m_cBkRemoved);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_REMOVED, cBk, cFg);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_IDENTICALREMOVED, cBk, cFg);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_THEIRSREMOVED, cBk, cFg);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_YOURSREMOVED, cBk, cFg);
+
+    cFg = GetColorFromButton(m_cFgAdded);
+    cBk = GetColorFromButton(m_cBkAdded);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_ADDED, cBk, cFg);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_IDENTICALADDED, cBk, cFg);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_THEIRSADDED, cBk, cFg);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_YOURSADDED, cBk, cFg);
+
+    if ((DWORD)m_regInlineAdded != (DWORD)m_cBkInlineAdded.GetColor())
+        m_bReloadNeeded = true;
+    m_regInlineAdded = GetColorFromButton(m_cBkInlineAdded);
+    if ((DWORD)m_regInlineRemoved != (DWORD)m_cBkInlineRemoved.GetColor())
+        m_bReloadNeeded = true;
+    m_regInlineRemoved = GetColorFromButton(m_cBkInlineRemoved);
+    if ((DWORD)m_regModifiedBackground != (DWORD)m_cBkModified.GetColor())
+        m_bReloadNeeded = true;
+    m_regModifiedBackground = GetColorFromButton(m_cBkModified);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_EDITED, (DWORD)m_regModifiedBackground, cFg);
+
+    cFg = GetColorFromButton(m_cFgEmpty);
+    cBk = GetColorFromButton(m_cBkEmpty);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_EMPTY, cBk, cFg);
+
+    COLORREF adjustedcolor = cBk;
+    cFg = GetColorFromButton(m_cFgConflict);
+    cBk = GetColorFromButton(m_cBkConflict);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_CONFLICTED, cBk, cFg);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_CONFLICTED_IGNORED, cBk, cFg);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_CONFLICTADDED, cBk, cFg);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_CONFLICTEMPTY, adjustedcolor, cFg);
+
+    cFg = GetColorFromButton(m_cFgConflictResolved);
+    cBk = GetColorFromButton(m_cBkConflictResolved);
+    CDiffColors::GetInstance().SetColors(DIFFSTATE_CONFLICTRESOLVED, cBk, cFg);
+
+    cFg = GetColorFromButton(m_cFgWhitespaces);
+    CRegDWORD regWhitespaceColor(L"Software\\TortoiseMerge\\Colors\\Whitespace", GetSysColor(COLOR_3DSHADOW));
+    regWhitespaceColor = cFg;
+}
+
+void CSetColorPage::DoDataExchange(CDataExchange* pDX)
+{
+    CPropertyPage::DoDataExchange(pDX);
+    DDX_Control(pDX, IDC_BKNORMAL, m_cBkNormal);
+    DDX_Control(pDX, IDC_BKREMOVED, m_cBkRemoved);
+    DDX_Control(pDX, IDC_BKADDED, m_cBkAdded);
+    DDX_Control(pDX, IDC_BKWHITESPACES, m_cBkInlineAdded);
+    DDX_Control(pDX, IDC_BKWHITESPACEDIFF, m_cBkInlineRemoved);
+    DDX_Control(pDX, IDC_BKMODIFIED, m_cBkModified);
+    DDX_Control(pDX, IDC_BKEMPTY, m_cBkEmpty);
+    DDX_Control(pDX, IDC_BKCONFLICTED, m_cBkConflict);
+    DDX_Control(pDX, IDC_BKCONFLICTRESOLVED, m_cBkConflictResolved);
+    DDX_Control(pDX, IDC_FGWHITESPACES, m_cFgWhitespaces);
+    DDX_Control(pDX, IDC_FGNORMAL, m_cFgNormal);
+    DDX_Control(pDX, IDC_FGREMOVED, m_cFgRemoved);
+    DDX_Control(pDX, IDC_FGADDED, m_cFgAdded);
+    DDX_Control(pDX, IDC_FGEMPTY, m_cFgEmpty);
+    DDX_Control(pDX, IDC_FGCONFLICTED, m_cFgConflict);
+    DDX_Control(pDX, IDC_FGCONFLICTRESOLVED, m_cFgConflictResolved);
+}
+
+
+BEGIN_MESSAGE_MAP(CSetColorPage, CPropertyPage)
+    ON_BN_CLICKED(IDC_BKNORMAL, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_BKREMOVED, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_BKADDED, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_BKWHITESPACES, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_BKWHITESPACEDIFF, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_BKMODIFIED, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_BKEMPTY, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_BKCONFLICTED, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_BKCONFLICTRESOLVED, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_FGNORMAL, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_FGREMOVED, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_FGADDED, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_FGEMPTY, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_FGCONFLICTED, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_FGCONFLICTRESOLVED, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_FGWHITESPACES, &CSetColorPage::OnBnClickedColor)
+    ON_BN_CLICKED(IDC_RESTORE, &CSetColorPage::OnBnClickedRestore)
+END_MESSAGE_MAP()
+
+
+// CSetColorPage message handlers
+
+BOOL CSetColorPage::OnInitDialog()
+{
+    CPropertyPage::OnInitDialog();
+
+    COLORREF cBk;
+    COLORREF cFg;
+
+    CString sDefaultText, sCustomText;
+    sDefaultText.LoadString(IDS_COLOURPICKER_DEFAULTTEXT);
+    sCustomText.LoadString(IDS_COLOURPICKER_CUSTOMTEXT);
+
+    CDiffColors::GetInstance().GetColors(DIFFSTATE_NORMAL, cBk, cFg);
+    m_cFgNormal.SetColor(cFg);
+    m_cFgNormal.EnableAutomaticButton(sDefaultText, DIFFSTATE_NORMAL_DEFAULT_FG);
+    m_cFgNormal.EnableOtherButton(sCustomText);
+    m_cBkNormal.SetColor(cBk);
+    m_cBkNormal.EnableAutomaticButton(sDefaultText, DIFFSTATE_NORMAL_DEFAULT_BG);
+    m_cBkNormal.EnableOtherButton(sCustomText);
+
+    CDiffColors::GetInstance().GetColors(DIFFSTATE_REMOVED, cBk, cFg);
+    m_cFgRemoved.SetColor(cFg);
+    m_cFgRemoved.EnableAutomaticButton(sDefaultText, DIFFSTATE_REMOVED_DEFAULT_FG);
+    m_cFgRemoved.EnableOtherButton(sCustomText);
+    m_cBkRemoved.SetColor(cBk);
+    m_cBkRemoved.EnableAutomaticButton(sDefaultText, DIFFSTATE_REMOVED_DEFAULT_BG);
+    m_cBkRemoved.EnableOtherButton(sCustomText);
+
+    CDiffColors::GetInstance().GetColors(DIFFSTATE_ADDED, cBk, cFg);
+    m_cFgAdded.SetColor(cFg);
+    m_cFgAdded.EnableAutomaticButton(sDefaultText, DIFFSTATE_ADDED_DEFAULT_FG);
+    m_cFgAdded.EnableOtherButton(sCustomText);
+    m_cBkAdded.SetColor(cBk);
+    m_cBkAdded.EnableAutomaticButton(sDefaultText, DIFFSTATE_ADDED_DEFAULT_BG);
+    m_cBkAdded.EnableOtherButton(sCustomText);
+
+    m_cBkInlineAdded.SetColor((DWORD)m_regInlineAdded);
+    m_cBkInlineAdded.EnableAutomaticButton(sDefaultText, INLINEADDED_COLOR);
+    m_cBkInlineAdded.EnableOtherButton(sCustomText);
+
+    m_cBkInlineRemoved.SetColor((DWORD)m_regInlineRemoved);
+    m_cBkInlineRemoved.EnableAutomaticButton(sDefaultText, INLINEREMOVED_COLOR);
+    m_cBkInlineRemoved.EnableOtherButton(sCustomText);
+
+    m_cBkModified.SetColor((DWORD)m_regModifiedBackground);
+    m_cBkModified.EnableAutomaticButton(sDefaultText, MODIFIED_COLOR);
+    m_cBkModified.EnableOtherButton(sCustomText);
+
+    CDiffColors::GetInstance().GetColors(DIFFSTATE_EMPTY, cBk, cFg);
+    m_cFgEmpty.SetColor(cFg);
+    m_cFgEmpty.EnableAutomaticButton(sDefaultText, DIFFSTATE_EMPTY_DEFAULT_FG);
+    m_cFgEmpty.EnableOtherButton(sCustomText);
+    m_cBkEmpty.SetColor(cBk);
+    m_cBkEmpty.EnableAutomaticButton(sDefaultText, DIFFSTATE_EMPTY_DEFAULT_BG);
+    m_cBkEmpty.EnableOtherButton(sCustomText);
+
+    CDiffColors::GetInstance().GetColors(DIFFSTATE_CONFLICTED, cBk, cFg);
+    m_cFgConflict.SetColor(cFg);
+    m_cFgConflict.EnableAutomaticButton(sDefaultText, DIFFSTATE_CONFLICTED_DEFAULT_FG);
+    m_cFgConflict.EnableOtherButton(sCustomText);
+    m_cBkConflict.SetColor(cBk);
+    m_cBkConflict.EnableAutomaticButton(sDefaultText, DIFFSTATE_CONFLICTED_DEFAULT_BG);
+    m_cBkConflict.EnableOtherButton(sCustomText);
+
+    CDiffColors::GetInstance().GetColors(DIFFSTATE_CONFLICTRESOLVED, cBk, cFg);
+    m_cFgConflictResolved.SetColor(cFg);
+    m_cFgConflictResolved.EnableAutomaticButton(sDefaultText, DIFFSTATE_CONFLICTRESOLVED_DEFAULT_FG);
+    m_cFgConflictResolved.EnableOtherButton(sCustomText);
+    m_cBkConflictResolved.SetColor(cBk);
+    m_cBkConflictResolved.EnableAutomaticButton(sDefaultText, DIFFSTATE_CONFLICTRESOLVED_DEFAULT_BG);
+    m_cBkConflictResolved.EnableOtherButton(sCustomText);
+
+    CRegDWORD regWhitespaceColor(L"Software\\TortoiseMerge\\Colors\\Whitespace", GetSysColor(COLOR_3DSHADOW));
+    m_cFgWhitespaces.SetColor((COLORREF)(DWORD)regWhitespaceColor);
+    m_cFgWhitespaces.EnableAutomaticButton(sDefaultText, GetSysColor(COLOR_3DSHADOW));
+    m_cFgWhitespaces.EnableOtherButton(sCustomText);
+
+    m_bInit = TRUE;
+
+    return TRUE;  // return TRUE unless you set the focus to a control
+    // EXCEPTION: OCX Property Pages should return FALSE
+}
+
+BOOL CSetColorPage::OnApply()
+{
+    UpdateData();
+    SaveData();
+    SetModified(FALSE);
+    return CPropertyPage::OnApply();
+}
+
+void CSetColorPage::OnBnClickedColor()
+{
+    SetModified();
+}
+
+void CSetColorPage::OnBnClickedRestore()
+{
+    m_cBkNormal.SetColor(DIFFSTATE_NORMAL_DEFAULT_BG);
+    m_cBkRemoved.SetColor(DIFFSTATE_REMOVED_DEFAULT_BG);
+    m_cBkAdded.SetColor(DIFFSTATE_ADDED_DEFAULT_BG);
+    m_cBkInlineAdded.SetColor(INLINEADDED_COLOR);
+    m_cBkInlineRemoved.SetColor(INLINEREMOVED_COLOR);
+    m_cBkModified.SetColor(MODIFIED_COLOR);
+    m_cBkEmpty.SetColor(DIFFSTATE_EMPTY_DEFAULT_BG);
+    m_cBkConflict.SetColor(DIFFSTATE_CONFLICTED_DEFAULT_BG);
+    m_cBkConflictResolved.SetColor(DIFFSTATE_CONFLICTRESOLVED_DEFAULT_BG);
+    m_cFgNormal.SetColor(DIFFSTATE_NORMAL_DEFAULT_FG);
+    m_cFgRemoved.SetColor(DIFFSTATE_REMOVED_DEFAULT_FG);
+    m_cFgAdded.SetColor(DIFFSTATE_ADDED_DEFAULT_FG);
+    m_cFgEmpty.SetColor(DIFFSTATE_EMPTY_DEFAULT_FG);
+    m_cFgConflict.SetColor(DIFFSTATE_CONFLICTED_DEFAULT_FG);
+    m_cFgConflictResolved.SetColor(DIFFSTATE_CONFLICTRESOLVED_DEFAULT_FG);
+    m_cFgWhitespaces.SetColor(GetSysColor(COLOR_3DSHADOW));
+    SetModified();
+}
---
+++ branches/shelve/www/Gruntfile.js
@@ -0,0 +1,332 @@
+'use strict';
+
+module.exports = function(grunt) {
+
+    grunt.initConfig({
+        dirs: {
+            dest: 'dist',
+            src: 'source'
+        },
+
+        jekyll: {
+            site: {
+                options: {
+                    bundleExec: true,
+                    incremental: false
+                }
+            }
+        },
+
+        htmlmin: {
+            dist: {
+                options: {
+                    collapseBooleanAttributes: true,
+                    collapseInlineTagWhitespace: false,
+                    collapseWhitespace: true,
+                    conservativeCollapse: false,
+                    decodeEntities: true,
+                    ignoreCustomComments: [/^\s*google(off|on):\s/],
+                    minifyCSS: {
+                        level: {
+                            1: {
+                                specialComments: 0
+                            }
+                        }
+                    },
+                    minifyJS: true,
+                    minifyURLs: false,
+                    processConditionalComments: true,
+                    removeAttributeQuotes: true,
+                    removeComments: true,
+                    removeOptionalAttributes: true,
+                    removeOptionalTags: true,
+                    removeRedundantAttributes: true,
+                    removeScriptTypeAttributes: true,
+                    removeStyleLinkTypeAttributes: true,
+                    removeTagWhitespace: false,
+                    sortAttributes: true,
+                    sortClassName: true
+                },
+                expand: true,
+                cwd: '<%= dirs.dest %>',
+                dest: '<%= dirs.dest %>',
+                src: ['**/*.html']
+            }
+        },
+
+        concat: {
+            css: {
+                src: ['<%= dirs.src %>/assets/css/vendor/normalize.css',
+                      '<%= dirs.src %>/assets/css/vendor/baguetteBox.css',
+                      '<%= dirs.src %>/assets/css/vendor/highlighter.css',
+                      '<%= dirs.src %>/assets/css/flags-sprite.css',
+                      '<%= dirs.src %>/assets/css/style.css'
+                ],
+                dest: '<%= dirs.dest %>/assets/css/pack.css'
+            },
+            mainJs: {
+                src: ['<%= dirs.src %>/assets/js/vendor/plugins.js',
+                      '<%= dirs.src %>/assets/js/no-js-class.js',
+                      '<%= dirs.src %>/assets/js/onLoad.js',
+                      '<%= dirs.src %>/assets/js/sf-accel.js',
+                      '<%= dirs.src %>/assets/js/google-analytics.js'
+                ],
+                dest: '<%= dirs.dest %>/assets/js/main.js'
+            },
+            baguetteBox: {
+                src: ['<%= dirs.src %>/assets/js/vendor/baguetteBox.js',
+                      '<%= dirs.src %>/assets/js/baguetteBox-init.js'
+                ],
+                dest: '<%= dirs.dest %>/assets/js/baguetteBox-pack.js'
+            }
+        },
+
+        autoprefixer: {
+            options: {
+                browsers: [
+                    'last 2 versions',
+                    '> 1%',
+                    'Edge >= 12',
+                    'Explorer >= 9',
+                    'Firefox ESR'
+                ]
+            },
+            pack: {
+                src: '<%= concat.css.dest %>',
+                dest: '<%= concat.css.dest %>'
+            }
+        },
+
+        uncss: {
+            options: {
+                htmlroot: '<%= dirs.dest %>',
+                ignore: [
+                    /(#|\.)baguetteBox(-[a-zA-Z]+)?/,
+                    /\.no\-js/
+                ],
+                ignoreSheets: [/fonts.googleapis/, /www.google.com/, /pagead2.googlesyndication.com/],
+                stylesheets: ['/assets/css/pack.css']
+            },
+            dist: {
+                src: '<%= dirs.dest %>/**/*.html',
+                dest: '<%= concat.css.dest %>'
+            }
+        },
+
+        cssmin: {
+            minify: {
+                options: {
+                    level: {
+                        1: {
+                            specialComments: 0
+                        }
+                    }
+                },
+                files: {
+                    '<%= uncss.dist.dest %>': '<%= concat.css.dest %>'
+                }
+            }
+        },
+
+        uglify: {
+            options: {
+                compress: true,
+                mangle: true,
+                output: {
+                    comments: false
+                }
+            },
+            minify: {
+                files: {
+                    '<%= concat.baguetteBox.dest %>': '<%= concat.baguetteBox.dest %>',
+                    '<%= concat.mainJs.dest %>': '<%= concat.mainJs.dest %>'
+                }
+            }
+        },
+
+        filerev: {
+            css: {
+                src: '<%= dirs.dest %>/assets/css/**/*.css'
+            },
+            js: {
+                src: [
+                    '<%= dirs.dest %>/assets/js/**/*.js'
+                ]
+            },
+            images: {
+                src: [
+                    '<%= dirs.dest %>/assets/img/**/*.{jpg,jpeg,gif,png,svg}',
+                    '!<%= dirs.dest %>/assets/img/logo-256x256.png'
+                ]
+            }
+        },
+
+        useminPrepare: {
+            html: '<%= dirs.dest %>/index.html',
+            options: {
+                dest: '<%= dirs.dest %>',
+                root: '<%= dirs.dest %>'
+            }
+        },
+
+        usemin: {
+            css: '<%= dirs.dest %>/assets/css/pack*.css',
+            html: '<%= dirs.dest %>/**/*.html',
+            options: {
+                assetsDirs: ['<%= dirs.dest %>/', '<%= dirs.dest %>/assets/img/']
+            }
+        },
+
+        svgmin: {
+            options: {
+                plugins: [{
+                    removeViewBox: false
+                }, {
+                    removeUselessStrokeAndFill: false
+                }, {
+                    removeEmptyAttrs: false
+                }, {
+                    removeTitle: false
+                }]
+            },
+            dist: {
+                expand: true,
+                cwd: '<%= dirs.dest %>',
+                dest: '<%= dirs.dest %>',
+                src: 'assets/img/**/*.svg'
+            }
+        },
+
+        connect: {
+            options: {
+                base: '<%= dirs.dest %>/',
+                hostname: 'localhost'
+            },
+            livereload: {
+                options: {
+                    base: '<%= dirs.dest %>/',
+                    livereload: 35729,
+                    open: true,  // Automatically open the webpage in the default browser
+                    port: 8000
+                }
+            },
+            linkChecker: {
+                options: {
+                    base: '<%= dirs.dest %>/',
+                    port: 9001
+                }
+            }
+        },
+
+        watch: {
+            options: {
+                livereload: '<%= connect.livereload.options.livereload %>'
+            },
+            dev: {
+                files: ['<%= dirs.src %>/**', '.jshintrc', '_config.yml', 'Gruntfile.js'],
+                tasks: 'dev'
+            },
+            build: {
+                files: ['<%= dirs.src %>/**', '.jshintrc', '_config.yml', 'Gruntfile.js'],
+                tasks: 'build'
+            }
+        },
+
+        csslint: {
+            options: {
+                csslintrc: '.csslintrc'
+            },
+            src: '<%= dirs.src %>/assets/css/style.css'
+        },
+
+        jshint: {
+            options: {
+                jshintrc: '.jshintrc'
+            },
+            files: {
+                src: ['Gruntfile.js', '<%= dirs.src %>/assets/js/*.js', '!<%= dirs.src %>/assets/js/google-analytics.js']
+            }
+        },
+
+        htmllint: {
+            src: '<%= dirs.dest %>/**/*.html'
+        },
+
+        linkChecker: {
+            options: {
+                callback: function (crawler) {
+                    crawler.addFetchCondition(function (queueItem) {
+                        return !queueItem.path.match(/\/docs\/(release|nightly)\//) &&
+                               queueItem.path !== '/assets/js/vendor/g.src' &&
+                               queueItem.path !== '/assets/js/+f+' &&
+                               queueItem.path !== '/assets/js/%7Bhref%7D' &&
+                               queueItem.path !== '/a';
+                    });
+                },
+                interval: 1,        // 1 ms; default 250
+                maxConcurrency: 5   // default; bigger doesn't seem to improve time
+            },
+            dev: {
+                site: 'localhost',
+                options: {
+                    initialPort: '<%= connect.linkChecker.options.port %>'
+                }
+            }
+        },
+
+        clean: {
+            dist: '<%= dirs.dest %>/'
+        }
+
+    });
+
+    // Load any grunt plugins found in package.json.
+    require('load-grunt-tasks')(grunt, { scope: 'dependencies' });
+    require('time-grunt')(grunt);
+
+    grunt.registerTask('build', [
+        'clean',
+        'jekyll',
+        'useminPrepare',
+        'concat',
+        'autoprefixer',
+        'uncss',
+        'cssmin',
+        'uglify',
+        'filerev',
+        'usemin',
+        'svgmin',
+        'htmlmin'
+    ]);
+
+    grunt.registerTask('test', [
+        'build',
+        'csslint',
+        'jshint',
+        'htmllint',
+        'connect:linkChecker',
+        'linkChecker'
+    ]);
+
+    grunt.registerTask('dev', [
+        'jekyll',
+        'useminPrepare',
+        'concat',
+        'autoprefixer',
+        'filerev',
+        'usemin'
+    ]);
+
+    grunt.registerTask('server', [
+        'build',
+        'connect:livereload',
+        'watch:build'
+    ]);
+
+    grunt.registerTask('default', [
+        'dev',
+        'connect:livereload',
+        'watch:dev'
+    ]);
+
+};
