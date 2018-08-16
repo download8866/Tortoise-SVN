d738dd973bb8ebcb70b67f9d3ad823213dd1ac33
stefankueng <stefankueng@154be670-7969-47af-9ab0-c6fb378d2295>
2018-08-16 14:34:50 +0000

    Merged revision(s) r28330, r28334 from trunk: * make the icon borders and min heights of the splitters in the log dialog dpi aware. * When restoring files, adjust their last-write-time to the current time.

--- src/TortoiseProc/CommitDlg.cpp
+++ src/TortoiseProc/CommitDlg.cpp
@@ -1,6 +1,6 @@
 ﻿// TortoiseSVN - a Windows shell extension for easy version control
 
-// Copyright (C) 2003-2017 - TortoiseSVN
+// Copyright (C) 2003-2018 - TortoiseSVN
 
 // This program is free software; you can redistribute it and/or
 // modify it under the terms of the GNU General Public License
@@ -769,6 +769,7 @@ void CCommitDlg::OnCancel()
         for (auto it = m_restorepaths.cbegin(); it != m_restorepaths.cend(); ++it)
         {
             CopyFile(it->first, std::get<0>(it->second), FALSE);
+            CPathUtils::Touch(std::get<0>(it->second));
             svn.AddToChangeList(CTSVNPathList(CTSVNPath(std::get<0>(it->second))), std::get<1>(it->second), svn_depth_empty);
         }
     }
