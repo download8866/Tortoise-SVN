707d7b4e1f9191916f7c5005cfab8e45d2738145
stefankueng <stefankueng@154be670-7969-47af-9ab0-c6fb378d2295>
2019-01-07 18:58:20 +0000

    tag the 1.10.3 release

---
+++ tags/version-1.10.3/src/TortoiseShell/ItemIDList.cpp
@@ -0,0 +1,143 @@
+// TortoiseSVN - a Windows shell extension for easy version control
+
+// Copyright (C) 2016 - TortoiseGit
+// Copyright (C) 2003-2006, 2009, 2011-2016 - TortoiseSVN
+
+// This program is free software; you can redistribute it and/or
+// modify it under the terms of the GNU General Public License
+// as published by the Free Software Foundation; either version 2
+// of the License, or (at your option) any later version.
+
+// This program is distributed in the hope that it will be useful,
+// but WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+// GNU General Public License for more details.
+
+// You should have received a copy of the GNU General Public License
+// along with this program; if not, write to the Free Software Foundation,
+// 51 Franklin Street, Fifth Floor, Boston, MA 02110-1301 USA.
+//
+#include "stdafx.h"
+#include "ShellExt.h"
+#include "ItemIDList.h"
+
+
+ItemIDList::ItemIDList(PCUITEMID_CHILD item, PCUIDLIST_RELATIVE parent)
+    : item_ (item)
+    , parent_ (parent)
+    , count_ (-1)
+{
+}
+
+ItemIDList::ItemIDList( PCIDLIST_ABSOLUTE item )
+    : item_ ((PCUITEMID_CHILD)item)
+    , parent_ (0)
+    , count_ (-1)
+{
+
+}
+
+ItemIDList::~ItemIDList()
+{
+}
+
+int ItemIDList::size() const
+{
+    if (count_ == -1)
+    {
+        count_ = 0;
+        if (item_)
+        {
+            LPCSHITEMID ptr = &item_->mkid;
+            while (ptr != 0 && ptr->cb != 0)
+            {
+                ++count_;
+                LPBYTE byte = (LPBYTE) ptr;
+                byte += ptr->cb;
+                ptr = (LPCSHITEMID) byte;
+            }
+        }
+    }
+    return count_;
+}
+
+LPCSHITEMID ItemIDList::get(int index) const
+{
+    int count = 0;
+
+    if (item_ == NULL)
+        return NULL;
+    LPCSHITEMID ptr = &item_->mkid;
+    if (ptr == NULL)
+        return NULL;
+    while (ptr->cb != 0)
+    {
+        if (count == index)
+            break;
+
+        ++count;
+        LPBYTE byte = (LPBYTE) ptr;
+        byte += ptr->cb;
+        ptr = (LPCSHITEMID) byte;
+    }
+    return ptr;
+
+}
+
+tstring ItemIDList::toString(bool resolveLibraries /*= true*/)
+{
+    CComPtr<IShellFolder> shellFolder;
+    CComPtr<IShellFolder> parentFolder;
+    tstring ret;
+
+    if (FAILED(::SHGetDesktopFolder(&shellFolder)))
+        return ret;
+    if (!parent_ || FAILED(shellFolder->BindToObject(parent_, 0, IID_IShellFolder, (void**)&parentFolder)))
+        parentFolder = shellFolder;
+
+    STRRET name;
+    TCHAR* szDisplayName = nullptr;
+    if ((parentFolder != 0)&&(item_ != 0))
+    {
+        if (FAILED(parentFolder->GetDisplayNameOf(item_, SHGDN_NORMAL | SHGDN_FORPARSING, &name)))
+            return ret;
+        if (FAILED(StrRetToStr(&name, item_, &szDisplayName)))
+            return ret;
+    }
+    if (!szDisplayName)
+        return ret;
+    ret = szDisplayName;
+    CoTaskMemFree(szDisplayName);
+
+    if (!((resolveLibraries) && (wcsncmp(ret.c_str(), L"::{", 3)==0)))
+        return ret;
+
+    CComPtr<IShellLibrary> plib;
+    if (FAILED(plib.CoCreateInstance(CLSID_ShellLibrary, nullptr, CLSCTX_INPROC_SERVER)))
+        return ret;
+
+    CComPtr<IShellItem> psiLibrary;
+    if (FAILED(SHCreateItemFromParsingName(ret.c_str(), nullptr, IID_PPV_ARGS(&psiLibrary))))
+        return ret;
+
+    if (FAILED(plib->LoadLibraryFromItem(psiLibrary, STGM_READ | STGM_SHARE_DENY_NONE)))
+        return ret;
+
+    CComPtr<IShellItem> psiSaveLocation;
+    if (FAILED(plib->GetDefaultSaveFolder(DSFT_DETECT, IID_PPV_ARGS(&psiSaveLocation))))
+        return ret;
+
+    PWSTR pszName = nullptr;
+    if (SUCCEEDED(psiSaveLocation->GetDisplayName(SIGDN_FILESYSPATH, &pszName)))
+    {
+        ret = pszName;
+        CoTaskMemFree(pszName);
+    }
+
+    return ret;
+}
+
+PCUITEMID_CHILD ItemIDList::operator& ()
+{
+    return item_;
+}
